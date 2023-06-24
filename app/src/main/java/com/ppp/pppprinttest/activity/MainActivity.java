9ce90e08724c524b18d54e6c34ad6db6f3f66f39
ppp <ppp.3ppj@gmail.com>
2023-06-24 17:56:59 +0700

    add test print text

---
+++ app/src/main/java/com/ppp/pppprinttest/activity/MainActivity.java
@@ -0,0 +1,41 @@
+package com.ppp.pppprinttest.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.ppp.pppprinttest.R;
+import com.ppp.pppprinttest.utils.SunmiPrintHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mButtonTestPrint;
+    TextView mTextViewPrinterStatus;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        mButtonTestPrint = findViewById(R.id.btnTestPrint);
+        mTextViewPrinterStatus = findViewById(R.id.tvText);
+        mButtonTestPrint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int status_printer  = SunmiPrintHelper.getInstance().sunmiPrinter;
+                Log.d("ppp", String.valueOf(status_printer));
+                mTextViewPrinterStatus.setText(String.valueOf(status_printer));
+                SunmiPrintHelper.getInstance().printText("Hi! ppp\n", 18, true, false, null);
+                SunmiPrintHelper.getInstance().printText("Hi! >.<", 20, true, false, null);
+                SunmiPrintHelper.getInstance().cutpaper();
+                Log.d("ppp", "Hii");
+            }
+        });
+    }
+    private void init(){
+        SunmiPrintHelper.getInstance().initSunmiPrinterService(this);
+    }
+}
